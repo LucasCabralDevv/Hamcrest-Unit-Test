4f9a58ac28b14b0bad9ff8f446c833e26b90ba42
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-24 12:32:05 -0300

    Terceiro teste realizado com sucesso.

--- app/src/test/java/com/lucascabral/hamcrestunittest/StatisticsTasksTest.kt
+++ app/src/test/java/com/lucascabral/hamcrestunittest/StatisticsTasksTest.kt
@@ -34,4 +34,20 @@ class StatisticsTasksTest {
         assertThat(result.completedTasksPercent, `is` (100f))
         assertThat(result.activeTasksPercent, `is` (0f))
     }
+
+    @Test
+    fun `test when getActiveAndCompletedStats returns forty percent of completed tasks`() {
+        val tasksList = listOf(
+            Task("title1", "desc1", isCompleted = true),
+            Task("title2", "desc2", isCompleted = true),
+            Task("title3", "desc3", isCompleted = true),
+            Task("title4", "desc4", isCompleted = false),
+            Task("title5", "desc5", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasksList)
+
+        assertThat(result.completedTasksPercent, `is` (60f))
+        assertThat(result.activeTasksPercent, `is` (40f))
+    }
 }
